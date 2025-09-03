1c7e2edf737d811b276fe3e69ad0c24592cc490b
abdulazeez2247 <abdulazeezoladokun65@gmail.com>
2025-09-03 10:49:54 +0100

    change1

--- src/data/api.js
+++ src/data/api.js
@@ -1,18 +1,26 @@
 import axios from "axios";
+
 const DASHBOARD_API_BASE_URL = "https://reedstreamsbackend1.onrender.com/api/dashboard";
+const MATCHES_API_BASE_URL = "https://reedstreamsbackend1.onrender.com/api/matches";
+
+
 export const getLiveStats = async () => {
   const res = await axios.get(`${DASHBOARD_API_BASE_URL}/live-stats`);
   return res.data.data;
 };
+
 export const getStreamsPerDay = async () => {
   const res = await axios.get(`${DASHBOARD_API_BASE_URL}/streams-per-day`);
   return res.data.data;
 };
+
 export const getMostStreamedSports = async () => {
   const res = await axios.get(`${DASHBOARD_API_BASE_URL}/most-streamed-sports`);
   return res.data.data;
 };
+
+
 export const getLiveMatchesBySport = async (sportId) => {
-  const res = await axios.get(`${DASHBOARD_API_BASE_URL}/matches?sport_id=${sportId}`);
+  const res = await axios.get(`${MATCHES_API_BASE_URL}?sport_id=${sportId}`);
   return res.data.matches || [];
 };
