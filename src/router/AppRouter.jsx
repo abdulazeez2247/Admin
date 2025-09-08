95ff7ffa0f6b4d7df30ec7cfd943b4bc741c4a45
abdulazeez2247 <abdulazeezoladokun65@gmail.com>
2025-09-09 00:39:51 +0100

    change10

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,34 +1,44 @@
-import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Dashboard from '../pages/Dashboard';
-import LiveMatches from '../pages/LiveMatches';
-import AdminLoginPage from '../pages/AdminLoginPage';
-import DashboardLayout from '../layouts/DashboardLayout';
+// import React, { useState } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+// import Dashboard from '../pages/Dashboard';
+// import LiveMatches from '../pages/LiveMatches';
+// import AdminLoginPage from '../pages/AdminLoginPage';
+// import DashboardLayout from '../layouts/DashboardLayout';
 
-const AppRouter = () => {
-  const [selectedSport, setSelectedSport] = useState(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(true); 
+// const AppRouter = () => {
+//   const [selectedSport, setSelectedSport] = useState(null);
+//   const [isAuthenticated, setIsAuthenticated] = useState(true); 
 
-  return (
-    <Routes>
-      <Route path="/" element={<AdminLoginPage setIsAuthenticated={setIsAuthenticated} />} />
+//   return (
+//     <Routes>
+//       <Route path="/" element={<AdminLoginPage setIsAuthenticated={setIsAuthenticated} />} />
 
      
-      {isAuthenticated && (
-        <Route element={<DashboardLayout setSelectedSport={setSelectedSport} />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route
-            path="/live-matches"
-            element={<LiveMatches selectedSportId={selectedSport} />}
-          />
-        </Route>
-      )}
+//       {isAuthenticated && (
+//         <Route element={<DashboardLayout setSelectedSport={setSelectedSport} />}>
+//           <Route path="/dashboard" element={<Dashboard />} />
+//           <Route
+//             path="/live-matches"
+//             element={<LiveMatches selectedSportId={selectedSport} />}
+//           />
+//         </Route>
+//       )}
 
-      {!isAuthenticated && (
-        <Route path="*" element={<AdminLoginPage setIsAuthenticated={setIsAuthenticated} />} />
-      )}
-    </Routes>
-  );
-};
+//       {!isAuthenticated && (
+//         <Route path="*" element={<AdminLoginPage setIsAuthenticated={setIsAuthenticated} />} />
+//       )}
+//     </Routes>
+//   );
+// };
 
-export default AppRouter;
+// export default AppRouter;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./router/AppRouter";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AppRouter />
+  </BrowserRouter>
+);
