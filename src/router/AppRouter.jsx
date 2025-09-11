30ccbc6403fcbf3614a2924fc79b145cdafebfe4
abdulazeez2247 <abdulazeezoladokun65@gmail.com>
2025-09-11 13:03:33 +0100

    change12

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,1 +1,35 @@
-change11
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import LiveMatches from '../pages/LiveMatches';
+import AdminLoginPage from '../pages/AdminLoginPage';
+import DashboardLayout from '../layouts/DashboardLayout';
+
+const AppRouter = () => {
+  const [selectedSport, setSelectedSport] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(true); 
+
+  return (
+    <Routes>
+      <Route path="/" element={<AdminLoginPage setIsAuthenticated={setIsAuthenticated} />} />
+
+     
+      {isAuthenticated && (
+        <Route element={<DashboardLayout setSelectedSport={setSelectedSport} />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/live-matches"
+            element={<LiveMatches selectedSportId={selectedSport} />}
+          />
+        </Route>
+      )}
+
+      {!isAuthenticated && (
+        <Route path="*" element={<AdminLoginPage setIsAuthenticated={setIsAuthenticated} />} />
+      )}
+    </Routes>
+  );
+};
+
+export default AppRouter;
+
