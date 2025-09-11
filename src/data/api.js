ca917e5a2baa738d3cf3fe9b3e1ebe7446e0a7e8
abdulazeez2247 <abdulazeezoladokun65@gmail.com>
2025-09-11 13:07:58 +0100

    matches

--- src/data/api.js
+++ src/data/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const DASHBOARD_API_BASE_URL = "https://reedstreams-backend.onrender.com/api/dashboard";
+const DASHBOARD_API_BASE_URL = "https://reedstreams-backend.onrender.com/api/matches";
 const MATCHES_API_BASE_URL = "https://reedstreams-backend.onrender.com/api/matches";
 
 export const getLiveStats = async () => {
